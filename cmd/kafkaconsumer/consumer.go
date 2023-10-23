bb72ca7adc1e1a843697efcc35f23e1fc76dba68
Hulla-HooP <shomaaaaaa76@gmail.com>
2023-10-23 21:35:03 +0300

    v 10

--- cmd/kafkaconsumer/consumer.go
+++ cmd/kafkaconsumer/consumer.go
@@ -25,8 +25,8 @@ type Color string
 func main() {
 
 	//Инициализируем логеры
-	infLogger := log.New(os.Stdout, "\n\u001b[33m INFO:  ", log.Ldate|log.Lshortfile)
-	errLogger := log.New(os.Stdout, "\n\u001b[31m ERROR:  ", log.Ldate|log.Lshortfile)
+	infLogger := log.New(os.Stdout, "\n\u001b[33m  INFO:  ", log.Ldate|log.Lshortfile)
+	errLogger := log.New(os.Stdout, "\n\u001b[31m  ERROR:  ", log.Ldate|log.Lshortfile)
 
 	//Загружаем .env содержащий все конфиги
 	err := godotenv.Load()
